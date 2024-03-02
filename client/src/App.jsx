49399c7d3d6167f6a04eac2d0f40b8832cf57a57
Doheera-kosi <kupourevans99@gmail.com>
2024-03-02 01:49:59 +0000

    Created Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashbord from "./pages/Dashbord";
+import Projects from "./pages/Projects";
+import SignOut from "./pages/SignOut";
+
 export default function App() {
-  return <h1 className="text-3xl text-red-500">Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashbord />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+Route;
