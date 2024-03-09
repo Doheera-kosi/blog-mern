757d8eee56391d790ce05573e58f6401cf939e95
Doheera-kosi <kupourevans99@gmail.com>
2024-03-09 00:07:52 +0000

    feat: Add PostPage component and update styles in App, and Complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,8 @@ import Dashboard from "./pages/Dashbord";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+
 
 
 export default function App() {
@@ -32,6 +34,7 @@ export default function App() {
         </Route>
 
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
         <Route path="/sign-out" element={<SignOut />} />
       </Routes>
       <Footer />
