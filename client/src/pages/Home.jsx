d7bc8038c9aa9337d6ab9d8a61e9dc30d30aa84e
Doheera-kosi <kupourevans99@gmail.com>
2024-03-13 13:00:11 +0000

    Update Home.jsx and Search.jsx components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import { useEffect, useState } from "react";
-import PostCard from '../components/PostCard'
+import PostCard from "../components/PostCard";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -35,18 +35,19 @@ export default function Home() {
         <CallToAction></CallToAction>
       </div>
 
-      <div className="max-w-6xl mx-auto p3 flex flex-col gap-8 py-7">
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6 mx-auto">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
-              {
-                posts.map((post) => (
-                  <PostCard key={post._id} post={post} />
-                ))
-              }
+            <div className="flex flex-wrap justify-center gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
             </div>
-            <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
               View all posts
             </Link>
           </div>
